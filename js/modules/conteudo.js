89d9361e9d8d7d6d2c21bc7afdf561b7a265f123
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2023-08-07 22:24:14 -0300

    conteudo teste

---
+++ js/modules/conteudo.js
@@ -0,0 +1,17 @@
+function conteudo(animalURL) {
+  let main = document.querySelector('.conteudo');
+  carregar(animalURL, main);
+}
+
+function carregar(animalURL, main) {
+  if (window.XMLHttpRequest) {
+     xmlhttp = new XMLHttpRequest();
+  }
+   xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      main.innerHTML = xmlhttp.responseText;
+    }
+  }
+  xmlhttp.open('GET', animalURL);
+  xmlhttp.send();
+}
