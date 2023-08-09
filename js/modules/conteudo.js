56fcf0ea82e719c060741e8b547960425bd9d984
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2023-08-09 20:10:09 -0300

    vaca conteudo

--- js/modules/conteudo.js
+++ js/modules/conteudo.js
@@ -6,6 +6,8 @@ function conteudo(animalURL) {
 function carregar(animalURL, main) {
   if (window.XMLHttpRequest) {
      xmlhttp = new XMLHttpRequest();
+  } else {
+    xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
   }
    xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
